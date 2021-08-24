b3144abcddf14b33a269bf8c94458bad576164d5
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-08-24 20:33:31 +0200

    more code

--- src/main/scala/example/ListOps.scala
+++ src/main/scala/example/ListOps.scala
@@ -1,10 +1,14 @@
 package example
 
 object ListOps {
-
   def isort(xs: List[Int]): List[Int] = xs match {
     case List() => List()
     case x :: xs1 => insert(x, isort(xs1))
+  }
+
+  def append[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List() => ys
+    case x :: xs1 => x :: append(xs1, ys)
   }
 
   private def insert(x: Int, xs: List[Int]): List[Int] = xs match {
--- src/test/scala/example/ListOpsSpec.scala
+++ src/test/scala/example/ListOpsSpec.scala
@@ -1,6 +1,6 @@
 package example
 
-import example.ListOps.isort
+import example.ListOps.{append, isort}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -16,6 +16,16 @@ class ListOpsSpec  extends AnyFlatSpec with Matchers {
     val numbers = List(20, 11, 12, 4)
     val result = isort(numbers)
     val expected = List(4, 11, 12, 20)
+    result shouldEqual expected
+  }
+
+  "List(2, 4) append List(22, 20)" should "return List(2, 4, 22, 20)" in {
+    val list1 = List(2, 4)
+    val list2 = List(22, 20)
+
+    val expected = List(2, 4, 22, 20)
+    val result: List[Int] = append[Int](list1, list2)
+
     result shouldEqual expected
   }
 }
